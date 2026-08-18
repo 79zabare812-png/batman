f51c74181b1676c8518f051c267207b51c40e3cc
unknown <79zabare812@gmail.com>
2026-08-18 11:50:53 +0300

    batman

--- index.js
+++
@@ -1,5 +0,0 @@
-const array = ["chamak", "aka" ,"ice spice", "fake person", "nauty","gay","simp","liar","thinks he/she is cool","ps5"];
-
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    console.log(element); }
